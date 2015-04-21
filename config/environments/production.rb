1259bc71b0922d350215c147d8722f8fecee4731
Ivana Rodriguez <ivanaramaaa@gmail.com>
2015-04-21 19:38:45 -0400

    Fix typo in production environment config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,11 @@
 Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.delivery_method = :smtp
-  host = 'https://secret-reef-9131.herokuapp.com'
+
+  host = 'https://conference-registration.herokuapp.com'
+  
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
